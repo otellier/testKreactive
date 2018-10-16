6d19bc28f5ce32f0b56a2de98ccc34978eccdb96
iOs kreactive <ioskreactive@Teds-borrowed-MacBook-Pro.local>
2018-10-16 16:54:35 +0200

    End of test

---
+++ Test Olivier TellierTests/Test_Olivier_TellierTests.swift
@@ -0,0 +1,36 @@
+//
+//  Test_Olivier_TellierTests.swift
+//  Test Olivier TellierTests
+//
+//  Created by iOs kreactive on 15/10/2018.
+//  Copyright © 2018 iOs kreactive. All rights reserved.
+//
+
+import XCTest
+@testable import Test_Olivier_Tellier
+
+class Test_Olivier_TellierTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
